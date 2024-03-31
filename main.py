f68ce5f2ab22f195305f10f34fa863a2ce887ade
lolovlad <lolovlad33@gmail.com>
2024-03-31 13:20:16 +0400

    update

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
+from Classes.ServerMonitoring import ServerMonitoring
+from Classes.ServerRepository import ServerRepository
+from settings import settings
+from time import sleep
 import psutil
 
 
-def get_fan_speeds():
-    # возвращает список скоростей вращения вентиляторов в RPM
-    return psutil.sensors_fans().values()
+repository = ServerRepository(settings.server_host, settings.server_port)
+
+s = ServerMonitoring()
+type_point_list = repository.get_all_type_point()
+
+key_type_point = {}
+
+for type_point in type_point_list:
+    key_type_point[type_point["name"]] = type_point["id"]
+
+print(key_type_point["CPU_temperature"])
+print(key_type_point["CPU_load"])
+print(key_type_point["disk_size"])
 
 
-def get_cpu_load():
-    # возвращает текущую загрузку CPU в процентах
-    return psutil.cpu_percent()
+token = repository.login_pc(settings.login, settings.password)["access_token"]
+repository.token = token
 
 
 while True:
-    print(psutil.sensors_temperatures())
-    print(get_fan_speeds())
+    space, free = s.disk_size()
+    load = s.cpu_load()
+    temp = s.cpu_temp()
+
+    space = round(space / (1024 * 1024 * 1024), 2)
+    free = round(free / (1024 * 1024 * 1024), 2)
+
+    print(f"CPU_temperature: {temp}")
+    print(f"disk_size: {space}  free_size: {free}")
+    print(f"CPU_load: {load}")
+
+    repository.post_point(str(temp), key_type_point["CPU_temperature"])
+    repository.post_point(str(space), key_type_point["disk_size"], str(free))
+    repository.post_point(str(load), key_type_point["CPU_load"])
+
+    sleep(settings.time_sleep)
+
